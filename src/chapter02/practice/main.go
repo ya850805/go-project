2aa3b11da2ce6c257016a299b5b7def54a553c1e
Jason <ya850805@gmail.com>
2023-01-09 01:55:28 +0800

    feat: Add practice for chapter2.

---
+++ src/chapter02/practice/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//1. 編寫打印Hello,Golang!的程序
+	fmt.Println("Hello,Golang!")
+
+	//2. 將個人基本信息(姓名、性別、籍貫、住址)打印出來。各條信息分別佔一行
+	fmt.Println("姓名\t性別\t籍貫\t住址\nJason\tmale\tTaiwan\tTaipei")
+}
