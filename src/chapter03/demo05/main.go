bf73ad218b7e5dfc265513dc7c85c7d28debb907
Jason <ya850805@gmail.com>
2023-01-10 01:43:01 +0800

    feat: Add plus operation demo.

---
+++ src/chapter03/demo05/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i = 1
+	var j = 2
+	//做加法運算
+	fmt.Println("i+j=", i+j)
+
+	var str1 = "hello"
+	var str2 = "world"
+	//做拼接操作
+	fmt.Println("str1+str2=", str1+str2)
+}
