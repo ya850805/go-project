525493dd2a4a9aea8e6bb47604b48c31dcf45c7e
Jason <ya850805@gmail.com>
2023-01-10 00:47:55 +0800

    feat: Add 3 ways to use golang variable.

---
+++ src/chapter03/demo02/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	//1. 第一種：指定變數類型，聲明後若不賦值，使用默認值。
+	//int的默認值是0
+	var i int
+	fmt.Println("i=", i)
+
+	//第二種：根據值自行判定變數類型(**類型推導**)。
+	var num = 10.11
+	fmt.Println("num=", num)
+
+	//第三種：省略`var`，:warning:`:=`左側的變數不應該是已經聲明過的，否則會導致編譯錯誤。
+	//下面的方式等價於 var name string = "jason"
+	//:=的:不能省略，否則編譯錯誤
+	name := "jason"
+	fmt.Println("name=", name)
+}
