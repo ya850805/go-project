eb36d0fa6c042f25bbbdee48b908d31c4b919693
Jason <ya850805@gmail.com>
2023-01-10 01:05:23 +0800

    feat: Add golang declare multiple global variables.

--- src/chapter03/demo02/main.go
+++ src/chapter03/demo02/main.go
@@ -13,7 +13,7 @@ func main() {
 	fmt.Println("num=", num)
 
 	//第三種：省略`var`，:warning:`:=`左側的變數不應該是已經聲明過的，否則會導致編譯錯誤。
-	//下面的方式等價於 var name string = "jason"
+	//下面的方式等價於 var name = "jason"
 	//:=的:不能省略，否則編譯錯誤
 	name := "jason"
 	fmt.Println("name=", name)
--- src/chapter03/demo03/main.go
+++ src/chapter03/demo03/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+// 定義全局變數
+var globalN1 = 100
+var globalN2 = 200
+var globalName1 = "juan1"
+
+// 上面的聲明方式，也可以改成一次性聲明
+var (
+	globalN3    = 300
+	globalN4    = 400
+	globalName2 = "juan2"
+)
+
 func main() {
 	//golang如何一次性聲明多個變數
 	var n1, n2, n3 int
@@ -14,4 +26,8 @@ func main() {
 	//一次性聲明多個不同類型的變數(省略var)
 	num3, name2, num4 := 300, "jason2", 400
 	fmt.Println("num3=", num3, "name2=", name2, "num4=", num4)
+
+	//輸出全局變數
+	fmt.Println("globalN1=", globalN1, "globalN2=", globalN2, "globalName1=", globalName1)
+	fmt.Println("globalN3=", globalN3, "globalN4=", globalN4, "globalName2=", globalName2)
 }
