4da9c45a9125a5c0cbe164bcb6a38f7719c000cd
Jason <ya850805@gmail.com>
2023-01-12 01:13:56 +0800

    feat: Add golang Printf and unsafe.Sizeof demo.

--- src/chapter03/intdemo06/main.go
+++ src/chapter03/intdemo06/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 // golang整數類型使用
 func main() {
@@ -26,4 +29,12 @@ func main() {
 	//報錯：The value of '-1' (type untyped int) cannot be represented by the type byte
 	//var c byte = -1
 
+	var n1 = 100 //n1是什麼類型
+	//fmt.Printf() 可以用於做格式化輸出
+	fmt.Printf("n1的類型是%T \n", n1)
+
+	//如何在程序中查看某個變數的佔用字節大小和數據類型
+	var n2 int64 = 10
+	//unsafe.Sizeof() 是unsafe包裡面提供的函數，可以返回變數佔用的字節數
+	fmt.Printf("n2的類型是%T，n2佔用的字節數是%d", n2, unsafe.Sizeof(n2))
 }
