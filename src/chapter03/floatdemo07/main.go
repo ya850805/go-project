103bc16dcf5d5112d4c9952131791e410eba9b64
Jason <ya850805@gmail.com>
2023-01-12 01:41:59 +0800

    feat: Add float loss of precision demo.

--- src/chapter03/floatdemo07/main.go
+++ src/chapter03/floatdemo07/main.go
@@ -10,4 +10,9 @@ func main() {
 	var num1 float32 = -0.00089
 	var num2 float64 = -7809656.09
 	fmt.Println("num1=", num1, "num2=", num2)
+
+	//尾數部分可能會丟失，造成精度損失。
+	var num3 float32 = -123.0000901
+	var num4 float64 = -123.0000901
+	fmt.Println("num3=", num3, "num4=", num4)
 }
