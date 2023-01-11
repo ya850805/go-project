0ae7b705f6551958d436600a2efe77e9fd7fdec7
Jason <ya850805@gmail.com>
2023-01-12 01:57:03 +0800

    feat: Add float scientific notation demo.

--- src/chapter03/floatdemo07/main.go
+++ src/chapter03/floatdemo07/main.go
@@ -24,4 +24,10 @@ func main() {
 	num6 := 5.12
 	num7 := .512 //0.512
 	fmt.Println("num6=", num6, "num7=", num7)
+
+	//科學計數形式
+	num8 := 5.1234e2   //5.1234 * 10的2次方
+	num9 := 5.1234e2   //5.1234 * 10的2次方
+	num10 := 5.1234e-2 //5.1234 / 10的2次方
+	fmt.Println("num8=", num8, "num9=", num9, "num10=", num10)
 }
