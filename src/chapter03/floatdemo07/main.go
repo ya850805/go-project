1f4ba320b0642d6ebdcf795b0bad64a306900022
Jason <ya850805@gmail.com>
2023-01-12 01:37:40 +0800

    feat: Add float demo.

---
+++ src/chapter03/floatdemo07/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// golang小數類型使用
+func main() {
+	var price float32 = 89.12
+	fmt.Println("price=", price)
+
+	var num1 float32 = -0.00089
+	var num2 float64 = -7809656.09
+	fmt.Println("num1=", num1, "num2=", num2)
+}
