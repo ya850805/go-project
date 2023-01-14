a7072df47e40f46ef47432a0f0c63329f854b26d
Jason <ya850805@gmail.com>
2023-01-15 01:45:04 +0800

    feat: Add golang string precautions demo.

--- src/chapter03/stringdemo10/main.go
+++ src/chapter03/stringdemo10/main.go
@@ -6,4 +6,31 @@ import "fmt"
 func main() {
 	var address = "台北市大安區"
 	fmt.Println("address=", address)
+
+	//報錯：Cannot assign to str[0]，字符串一但賦值了就不能修改了
+	//var str = "hello"
+	//str[0] = 'a'
+
+	str1 := "abc\nabc"
+	fmt.Println(str1)
+
+	//**反引號(\`)，以字符串的原生形式輸出，包括換行和特殊字符，可以實現防止攻擊、輸出源代碼等效果**
+	str2 := `
+		hello\n\t\\
+			123
+		456
+	`
+	fmt.Println(str2)
+
+	//字符串拼接
+	str3 := "hello" + " world"
+	fmt.Println("str3=", str3)
+	str3 += " haha"
+	fmt.Println("str3=", str3)
+
+	//當一個拼接的操作很長時，可以分行寫，需要將+號保留在上一行
+	str4 := "hello" + " world" + "hello" + " world" + "hello" + " world" +
+		"hello" + " world" +
+		"hello" + " world"
+	fmt.Println("str4=", str4)
 }
