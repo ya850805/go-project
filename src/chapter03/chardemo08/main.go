02192e0f11f15f5596f97d354c17799e6724c298
Jason <ya850805@gmail.com>
2023-01-15 01:01:18 +0800

    feat: Add %c and calculate by char demo.

--- src/chapter03/chardemo08/main.go
+++ src/chapter03/chardemo08/main.go
@@ -16,4 +16,16 @@ func main() {
 	//中文字如果用byte存由於對應的ascii code過大，因此會報錯overflow
 	var c3 = '北'
 	fmt.Printf("c3=%c c3對應的ascii code=%d \n", c3, c3)
+
+	//**字符的本質是一個整數**，直接輸出時，是該字符對應的UTF-8編碼的碼值
+	var c4 byte = '\n'
+	fmt.Println("c4=", c4)
+
+	//格式化%c輸出某個數，會輸出該數字對應的unicode字符
+	var c5 = 21271
+	fmt.Printf("c5=%c \n", c5)
+
+	//字符型是可以進行運算的，相當於一個整數，運算時是按照碼值運算
+	var n1 = 10 + 'a'
+	fmt.Printf("n1=%c", n1)
 }
