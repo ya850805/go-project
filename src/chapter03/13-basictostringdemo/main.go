2d94974aff83d3cf44a72f988a331ef5a43ed0a6
Jason <ya850805@gmail.com>
2023-01-16 01:26:02 +0800

    feat: Add strconv library that convert primitive type variable to string demo.

--- src/chapter03/13-basictostringdemo/main.go
+++ src/chapter03/13-basictostringdemo/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 // golang基本數據類型轉string
 func main() {
+	//第一種方式：`fmt.Sprintf()`
 	var num1 int = 10
 	var num2 float64 = 23.456
 	var b bool = true
@@ -11,7 +15,6 @@ func main() {
 
 	var str string //空的字符串
 
-	//第一種方式：`fmt.Sprintf()`
 	str = fmt.Sprintf("%d", num1)
 	fmt.Printf("str type is %T, str=%q \n", str, str)
 
@@ -23,4 +26,23 @@ func main() {
 
 	str = fmt.Sprintf("%c", myChar)
 	fmt.Printf("str type is %T, str=%q \n", str, str)
+
+	/////////////////////////////////////////////////////////////////////////////////////
+
+	//第二種方式：使用`strconv`包的函數
+	var num3 int = 99
+	var num4 float64 = 66.666
+	var b2 bool = true
+
+	var str2 string //空的字符串
+
+	str2 = strconv.FormatInt(int64(num3), 10)
+	fmt.Printf("str2 type is %T, str2=%q \n", str2, str2)
+
+	//說明：'f'：格式, 10：表示小數位保留10位, 64：表示這個小數是float64
+	str2 = strconv.FormatFloat(num4, 'f', 10, 64)
+	fmt.Printf("str2 type is %T, str2=%q \n", str2, str2)
+
+	str2 = strconv.FormatBool(b2)
+	fmt.Printf("str2 type is %T, str2=%q \n", str2, str2)
 }
