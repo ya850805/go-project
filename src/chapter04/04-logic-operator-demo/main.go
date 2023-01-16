602d0e8dc7bd1e8a9b3a898c468c1bb8566cd802
Jason <ya850805@gmail.com>
2023-01-17 01:06:48 +0800

    feat: Add golang logic operator precaution demo.

--- src/chapter04/04-logic-operator-demo/main.go
+++ src/chapter04/04-logic-operator-demo/main.go
@@ -29,4 +29,21 @@ func main() {
 	if !(age > 30) {
 		fmt.Println("ok6")
 	}
+
+	//短路與：**如果第一個條件為`false`，則第二個條件不會判斷，最終結果為`false`**
+	var i int = 10
+	if i < 9 && test() {
+		fmt.Println("ok7")
+	}
+
+	//短路或：**如果第一個條件為`true`，則第二個條件不會判斷，最終結果為`true`**
+	if i > 9 || test() {
+		fmt.Println("ok8")
+	}
+}
+
+// 測試函數
+func test() bool {
+	fmt.Println("test...")
+	return true
 }
