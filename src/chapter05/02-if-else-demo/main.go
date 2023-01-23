6e0868f681078a29d6c0bf885ce66bd8e7c16b5f
Jason <ya850805@gmail.com>
2023-01-24 02:22:54 +0800

    feat: Add golang if-else demo.

---
+++ src/chapter05/02-if-else-demo/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	//編寫一個程序，可以輸入人的年齡，
+	//如果這個人的年齡大於等於18歲，則輸出"你已經滿18歲，需要對自己的行為負責！"。
+	//否則輸出"你的年齡不大這次放過你了"
+
+	var age byte
+	fmt.Print("請輸入年齡：")
+	fmt.Scanln(&age)
+
+	if age >= 18 {
+		fmt.Println("你已經滿18歲，需要對自己的行為負責！")
+	} else {
+		fmt.Println("你的年齡不大這次放過你了")
+	}
+}
