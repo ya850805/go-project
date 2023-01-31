8e766abe96c635a4ce73d481ac2edd313cc075e8
Jason <ya850805@gmail.com>
2023-01-31 18:21:47 +0800

    feat: Add golang continue demo.

---
+++ src/chapter05/10-continue-demo/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//continue案例
+
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 10; j++ {
+			if j == 2 {
+				continue
+			}
+			fmt.Println("j=", j)
+		}
+	}
+}
