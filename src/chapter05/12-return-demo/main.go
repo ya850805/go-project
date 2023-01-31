f95196a5aa45aa6a08e722579e4cb6500c349cb8
Jason <ya850805@gmail.com>
2023-01-31 23:29:23 +0800

    feat: Add golang return demo.

---
+++ src/chapter05/12-return-demo/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// golang中的return使用案例
+func main() {
+	fmt.Println("ok1")
+	fmt.Println("ok2")
+	fmt.Println("ok3")
+	return
+	fmt.Println("ok4")
+	fmt.Println("ok5")
+	fmt.Println("ok6")
+	fmt.Println("ok7")
+}
