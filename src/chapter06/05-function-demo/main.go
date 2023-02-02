c1a819669d9f78673766147d044cad95aa819115
Jason <ya850805@gmail.com>
2023-02-03 00:32:35 +0800

    feat: Add golang function parameter contains a function demo.

--- src/chapter06/05-function-demo/main.go
+++ src/chapter06/05-function-demo/main.go
@@ -6,6 +6,11 @@ func getSum(n1 int, n2 int) int {
 	return n1 + n2
 }
 
+// 函數既然是一種數據類型，因此在Go中，函數可以做為參數，並且調用。
+func myFunc(funvar func(int, int) int, num1 int, num2 int) int {
+	return funvar(num1, num2)
+}
+
 func main() {
 	//在Go中，**函數也是一種數據類型**，可以賦值給一個變數，則該變數就是一個函數類型的變數了。通過該變數可以對函數調用
 	a := getSum
@@ -13,4 +18,7 @@ func main() {
 
 	res := a(10, 40) //等價getSum(10, 40)
 	fmt.Println("res=", res)
+
+	res2 := myFunc(getSum, 50, 60)
+	fmt.Println("res2=", res2)
 }
