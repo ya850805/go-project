0a4280cb3d64670dd76cb15a23feee455d3b4510
Jason <ya850805@gmail.com>
2023-02-04 17:26:19 +0800

    feat: Add golang time related function.

---
+++ src/chapter06/13-time-function-demo/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// golang中的time
+func main() {
+	//1. 獲取當前時間
+	now := time.Now()
+	fmt.Printf("now=%v, type=%T \n", now, now)
+
+	//2. 通過now可以獲得年月日時分秒
+	fmt.Printf("年=%v \n", now.Year())
+	fmt.Printf("月=%v \n", int(now.Month()))
+	fmt.Printf("日=%v \n", now.Day())
+	fmt.Printf("時=%v \n", now.Hour())
+	fmt.Printf("分=%v \n", now.Minute())
+	fmt.Printf("秒=%v \n", now.Second())
+
+	//格式化日期時間
+	//1)使用`Printf`或者`SPrintf`
+	fmt.Printf("當前時間：%d-%d-%d %d:%d:%d \n", now.Year(), int(now.Month()), now.Day(), now.Hour(), now.Minute(), now.Second())
+	dateStr := fmt.Sprintf("當前時間：%d-%d-%d %d:%d:%d \n", now.Year(), int(now.Month()), now.Day(), now.Hour(), now.Minute(), now.Second())
+	fmt.Println("dateStr=", dateStr)
+
+	//2)使用`.Format()`
+	fmt.Println(now.Format("2006/01/02 15:04:06"))
+	fmt.Println(now.Format("2006-01-02"))
+	fmt.Println(now.Format("15:04:06"))
+	fmt.Println("年：", now.Format("2006"))
+
+}
