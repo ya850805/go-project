167cc360ebf6b0f5b3b03d7779262bbac92ddaa2
Jason <ya850805@gmail.com>
2023-02-04 23:19:39 +0800

    feat: Add golang custom error and panic() demo.

--- src/chapter06/15-error-handling-demo/main.go
+++ src/chapter06/15-error-handling-demo/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 //golang的錯誤處理
 
@@ -21,7 +24,30 @@ func test() {
 	fmt.Println("res=", res)
 }
 
+// 模擬函數去讀取一個配置文件的信息
+// 如果文件名傳入不正確，就拋出一個自定義錯誤
+func readConf(name string) (err error) {
+	if name == "config.ini" {
+		return nil
+	} else {
+		//返回自定義錯誤
+		return errors.New("讀取文件錯誤")
+	}
+}
+
+func test02() {
+	err := readConf("configXXX.ini")
+	if err != nil {
+		//如果讀取文件函數返回錯誤，就輸出這個錯誤，並終止程序
+		panic(err)
+	}
+	fmt.Println("test02()繼續執行...")
+}
+
 func main() {
 	test()
 	fmt.Println("main下方的代碼")
+
+	//測試自定義錯誤
+	test02()
 }
