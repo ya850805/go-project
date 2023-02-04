5a3adc630941aa46dd609d6cc04990a06d38dd7d
Jason <ya850805@gmail.com>
2023-02-04 16:17:29 +0800

    feat: Add golang string related commonly used function - 2 & 3.

--- src/chapter06/12-string-function-demo/main.go
+++ src/chapter06/12-string-function-demo/main.go
@@ -58,4 +58,48 @@ func main() {
 	//返回子串在字符串第一次出現的index值，如果沒有，返回`-1`
 	index := strings.Index("NLT_abc", "abc")
 	fmt.Println("index=", index)
+
+	//返回子串在字符串最後一次出現的index值，如果沒有，返回`-1`
+	index = strings.LastIndex("go golang", "go")
+	fmt.Println("index=", index)
+
+	//將指定的子串替換成另一子串，可以指定替換幾個，如果`n=-1`表示全部替換
+	str = strings.Replace("go go hello", "go", "golang", 1)
+	fmt.Println("str=", str)
+
+	//按照指定的某個字符為分割標識，將一個字符串拆分成字符串數組
+	strArr := strings.Split("hello,world,ok", ",")
+	fmt.Println("strArr=", strArr)
+
+	//將字符串的字母進行大小寫的轉換
+	str = "goLang Hello"
+	lower := strings.ToLower(str)
+	upper := strings.ToUpper(str)
+	fmt.Println("str=", str)
+	fmt.Println("lower=", lower)
+	fmt.Println("upper=", upper)
+
+	//將字符串左右兩邊的空格去掉
+	str = strings.TrimSpace("    hello      ")
+	fmt.Printf("str=%s \n", str)
+
+	//將字符串左右兩邊指定的字符去掉
+	str = strings.Trim("! hello!", " !") //講左右兩邊的" "和!都去掉
+	fmt.Printf("str=%s \n", str)
+
+	//將字符串左邊指定的字符去掉
+	str = strings.TrimLeft("! hello!", " !")
+	fmt.Printf("str=%s \n", str)
+
+	//將字符串右邊指定的字符去掉
+	str = strings.TrimRight("! hello!", " !")
+	fmt.Printf("str=%s \n", str)
+
+	//判斷字符串是否已指定的字符串開頭
+	b = strings.HasPrefix("ftp://192.168.0.1", "ftp")
+	fmt.Println("b=", b)
+
+	//判斷字符串是否已指定的字符串結束
+	b = strings.HasSuffix("abc.jpg", "abc")
+	fmt.Println("b=", b)
 }
