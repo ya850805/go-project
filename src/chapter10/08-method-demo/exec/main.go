512cbe1dd64bab5e5480e6d1068056aee7014142
Jason <ya850805@gmail.com>
2023-02-14 16:09:06 +0800

    feat: Add more method exercise.

--- src/chapter10/08-method-demo/exec/main.go
+++ src/chapter10/08-method-demo/exec/main.go
@@ -5,7 +5,12 @@ import "fmt"
 type MethodUtils struct {
 }
 
-func (mu MethodUtils) Print() {
+type Calculator struct {
+	Num1 float64
+	Num2 float64
+}
+
+func (mu *MethodUtils) Print() {
 	for i := 0; i < 10; i++ {
 		for j := 0; j < 8; j++ {
 			fmt.Print("*")
@@ -14,7 +19,7 @@ func (mu MethodUtils) Print() {
 	}
 }
 
-func (mu MethodUtils) Print2(m int, n int) {
+func (mu *MethodUtils) Print2(m int, n int) {
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
 			fmt.Print("*")
@@ -23,8 +28,44 @@ func (mu MethodUtils) Print2(m int, n int) {
 	}
 }
 
-func (mu MethodUtils) area(len float64, width float64) float64 {
+func (mu *MethodUtils) Area(len float64, width float64) float64 {
 	return len * width
+}
+
+func (mu *MethodUtils) JudgeNum(num int) {
+	if num%2 == 0 {
+		fmt.Println(num, "是偶數")
+	} else {
+		fmt.Println(num, "是奇數")
+	}
+}
+
+func (mu *MethodUtils) Print3(n int, m int, key string) {
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			fmt.Print(key)
+		}
+		fmt.Println()
+	}
+}
+
+func (c *Calculator) cal(operator byte) float64 {
+	var res float64
+	n1 := c.Num1
+	n2 := c.Num2
+	switch operator {
+	case '+':
+		res = n1 + n2
+	case '-':
+		res = n1 - n2
+	case '*':
+		res = n1 * n2
+	case '/':
+		res = n1 / n2
+	default:
+		fmt.Println("運算符輸入有誤...")
+	}
+	return res
 }
 
 func main() {
@@ -40,6 +81,24 @@ func main() {
 	fmt.Println()
 
 	//3.編寫一個方法算矩形的面積(可以接收長`len`和寬`width`)，將其作為方法返回值。在`main()`方法中調用該方法，接收返回的面積值並打印。
-	area := methodUtils.area(10, 20)
+	area := methodUtils.Area(10, 20)
 	fmt.Println("面積為：", area)
+
+	fmt.Println()
+
+	//4. 編寫方法：判斷一個數是奇數還是偶數
+	methodUtils.JudgeNum(14)
+	methodUtils.JudgeNum(11)
+
+	fmt.Println()
+
+	//5. 根據行、列、字符打印對應行數和列數的字符，例如：行:3、列:2、字符:`*`，則打印相應的效果
+	methodUtils.Print3(3, 2, "#")
+
+	fmt.Println()
+
+	//定義一個計算器結構體，實現加減乘除四個功能，用一個方法完成，傳入一個運算符，結構體有2個數的屬性。
+	cal := Calculator{10, 5}
+	res := cal.cal('*')
+	fmt.Println("計算的結果：", res)
 }
